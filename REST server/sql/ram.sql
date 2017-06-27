846ecf176a6d659078380af8fcf335edd9eaf362
Duduf <git@duduf.com>
2017-06-27 11:18:13 +0200

    Use assets only #28

--- REST server/sql/ram.sql
+++ REST server/sql/ram.sql
@@ -1,11 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 27 Juin 2017 à 09:15
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `ram`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assets`
+--
 
 CREATE TABLE `assets` (
   `id` int(11) NOT NULL,
@@ -14,14 +33,27 @@ CREATE TABLE `assets` (
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assetstatuses`
+--
+
 CREATE TABLE `assetstatuses` (
   `id` int(11) NOT NULL,
   `stageId` int(11) NOT NULL,
   `assetId` int(11) NOT NULL,
   `statusId` int(11) DEFAULT NULL,
+  `shotId` int(11) NOT NULL,
   `comment` text NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projects`
+--
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
@@ -30,18 +62,25 @@ CREATE TABLE `projects` (
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projectstage`
+--
+
 CREATE TABLE `projectstage` (
   `id` int(11) NOT NULL,
   `stageId` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
-  `stageOrder` int(11) NOT NULL DEFAULT '0'
+  `stageOrder` int(11) NOT NULL DEFAULT '0',
+  `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `shotassets` (
-  `id` int(11) NOT NULL,
-  `shotId` int(11) NOT NULL,
-  `assetId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `shots`
+--
 
 CREATE TABLE `shots` (
   `id` int(11) NOT NULL,
@@ -52,128 +91,185 @@ CREATE TABLE `shots` (
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `shotstatuses` (
-  `id` int(11) NOT NULL,
-  `statusId` int(11) DEFAULT NULL,
-  `shotId` int(11) NOT NULL,
-  `stageId` int(11) NOT NULL,
-  `comment` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stages`
+--
 
 CREATE TABLE `stages` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `shortName` varchar(15) NOT NULL,
-  `type` varchar(1) NOT NULL
+  `type` varchar(1) NOT NULL,
+  `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `status`
+--
 
 CREATE TABLE `status` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `shortName` varchar(15) NOT NULL,
   `color` varchar(6) NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `latestUpdate` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `userName` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Index pour les tables exportées
+--
 
+--
+-- Index pour la table `assets`
+--
 ALTER TABLE `assets`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`shortName`,`name`);
 
+--
+-- Index pour la table `assetstatuses`
+--
 ALTER TABLE `assetstatuses`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `stage` (`stageId`,`assetId`),
   ADD KEY `assetId` (`assetId`),
-  ADD KEY `statusId` (`statusId`);
+  ADD KEY `statusId` (`statusId`),
+  ADD KEY `shotId` (`shotId`);
 
+--
+-- Index pour la table `projects`
+--
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`,`shortName`);
 
+--
+-- Index pour la table `projectstage`
+--
 ALTER TABLE `projectstage`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `projectStage` (`stageId`,`projectId`),
   ADD KEY `projectId` (`projectId`);
 
-ALTER TABLE `shotassets`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `asset` (`shotId`,`assetId`),
-  ADD KEY `assetId` (`assetId`);
-
+--
+-- Index pour la table `shots`
+--
 ALTER TABLE `shots`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`,`projectId`),
   ADD KEY `projectId` (`projectId`);
 
-ALTER TABLE `shotstatuses`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `status` (`statusId`,`shotId`,`stageId`),
-  ADD KEY `stageId` (`stageId`),
-  ADD KEY `shotId` (`shotId`);
-
+--
+-- Index pour la table `stages`
+--
 ALTER TABLE `stages`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`,`shortName`),
   ADD UNIQUE KEY `name_2` (`name`,`shortName`);
 
+--
+-- Index pour la table `status`
+--
 ALTER TABLE `status`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`,`shortName`);
 
+--
+-- Index pour la table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `userName` (`userName`);
 
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
 
+--
+-- AUTO_INCREMENT pour la table `assets`
+--
 ALTER TABLE `assets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `assetstatuses`
+--
 ALTER TABLE `assetstatuses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `projects`
+--
 ALTER TABLE `projects`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `projectstage`
+--
 ALTER TABLE `projectstage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `shotassets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `shots`
+--
 ALTER TABLE `shots`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `shotstatuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `stages`
+--
 ALTER TABLE `stages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `status`
+--
 ALTER TABLE `status`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
 
+--
+-- Contraintes pour la table `assetstatuses`
+--
 ALTER TABLE `assetstatuses`
   ADD CONSTRAINT `assetstatuses_ibfk_1` FOREIGN KEY (`stageId`) REFERENCES `stages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `assetstatuses_ibfk_2` FOREIGN KEY (`assetId`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `assetstatuses_ibfk_3` FOREIGN KEY (`statusId`) REFERENCES `status` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `assetstatuses_ibfk_3` FOREIGN KEY (`statusId`) REFERENCES `status` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `assetstatuses_ibfk_4` FOREIGN KEY (`shotId`) REFERENCES `shots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Contraintes pour la table `projectstage`
+--
 ALTER TABLE `projectstage`
   ADD CONSTRAINT `projectstage_ibfk_1` FOREIGN KEY (`stageId`) REFERENCES `stages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `projectstage_ibfk_2` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `shotassets`
-  ADD CONSTRAINT `shotassets_ibfk_1` FOREIGN KEY (`assetId`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shotassets_ibfk_2` FOREIGN KEY (`shotId`) REFERENCES `shots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+--
+-- Contraintes pour la table `shots`
+--
 ALTER TABLE `shots`
   ADD CONSTRAINT `shots_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE `shotstatuses`
-  ADD CONSTRAINT `shotstatuses_ibfk_1` FOREIGN KEY (`stageId`) REFERENCES `stages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shotstatuses_ibfk_2` FOREIGN KEY (`shotId`) REFERENCES `shots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shotstatuses_ibfk_3` FOREIGN KEY (`statusId`) REFERENCES `status` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
