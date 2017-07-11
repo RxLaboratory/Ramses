3b55d9f58269b14311899f3be80884ca5e217fae
Duduf <git@duduf.com>
2017-07-11 16:34:01 +0200

    changed to utf-8 and added uuids

--- REST server/sql/ram.sql
+++ REST server/sql/ram.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 07 Juillet 2017 à 16:09
+-- Généré le :  Mar 11 Juillet 2017 à 14:33
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -28,14 +28,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `assets` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `shortName` varchar(15) NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `shortName` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `stageId` int(11) NOT NULL,
   `statusId` int(11) DEFAULT NULL,
   `projectId` int(11) DEFAULT NULL,
-  `comment` text NOT NULL,
+  `comment` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -48,7 +49,7 @@ CREATE TABLE `assetstatuses` (
   `assetId` int(11) NOT NULL,
   `shotId` int(11) NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -58,10 +59,11 @@ CREATE TABLE `assetstatuses` (
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `shortName` varchar(15) NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `shortName` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -99,10 +101,11 @@ CREATE TABLE `projectstage` (
 
 CREATE TABLE `shots` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `duration` float NOT NULL DEFAULT '0',
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -112,11 +115,12 @@ CREATE TABLE `shots` (
 
 CREATE TABLE `stages` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `shortName` varchar(15) NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `shortName` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `autoCreateAssets` tinyint(1) NOT NULL DEFAULT '0',
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -126,24 +130,25 @@ CREATE TABLE `stages` (
 
 CREATE TABLE `status` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `shortName` varchar(15) NOT NULL,
-  `color` varchar(7) NOT NULL,
-  `description` text NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `shortName` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `color` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `status`
 --
 
-INSERT INTO `status` (`id`, `name`, `shortName`, `color`, `description`, `latestUpdate`) VALUES
-(1, 'Stand by', 'STB', '#6d6d6d', '', '2017-06-27 15:39:30'),
-(2, 'To do', 'TODO', '#00aaff', '', '2017-06-27 15:39:42'),
-(3, 'Retake', 'RTK', '#aa0000', '', '2017-06-27 15:39:52'),
-(4, 'OK', 'OK', '#00aa00', '', '2017-06-27 15:40:00'),
-(6, 'Check', 'CHK', '#ffaa00', '', '2017-06-27 15:41:49'),
-(7, 'Work in progress', 'WIP', '#d1bf61', '', '2017-07-04 14:12:50');
+INSERT INTO `status` (`id`, `uuid`, `name`, `shortName`, `color`, `description`, `latestUpdate`) VALUES
+(1, '', 'Stand by', 'STB', '#6d6d6d', '', '2017-06-27 15:39:30'),
+(2, '', 'To do', 'TODO', '#00aaff', '', '2017-06-27 15:39:42'),
+(3, '', 'Retake', 'RTK', '#aa0000', '', '2017-06-27 15:39:52'),
+(4, '', 'OK', 'OK', '#00aa00', '', '2017-06-27 15:40:00'),
+(6, '', 'Check', 'CHK', '#ffaa00', '', '2017-06-27 15:41:49'),
+(7, '', 'Work in progress', 'WIP', '#d1bf61', '', '2017-07-04 14:12:50');
 
 -- --------------------------------------------------------
 
@@ -153,19 +158,20 @@ INSERT INTO `status` (`id`, `name`, `shortName`, `color`, `description`, `latest
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `userName` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `firstName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `userName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `latestUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `firstName`, `lastName`, `userName`, `password`, `latestUpdate`) VALUES
-(1, '', '', 'Admin', 'd7e6961661419ce9e5650b51e3fd01ae8a97316ec840e465ec2254faf8f963390d928cb6dc9035a2fb1fbd8caa65def5c11fce4007c8d4814a14ec5f8b479f6e', '2017-07-07 15:55:57');
+INSERT INTO `users` (`id`, `uuid`, `firstName`, `lastName`, `userName`, `password`, `latestUpdate`) VALUES
+(1, '', '', '', 'Duduf', '7115e065d67c5949b9bf22490f6b793240191f6fc046791e5f4e91a1c0eb5d987b7d0c99fca9feb48ad416f58f375466a824ec6822bf1b253c28a5eb208895c0', '2017-07-11 14:32:55');
 
 --
 -- Index pour les tables exportées
@@ -176,6 +182,7 @@ INSERT INTO `users` (`id`, `firstName`, `lastName`, `userName`, `password`, `lat
 --
 ALTER TABLE `assets`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uuid` (`uuid`),
   ADD UNIQUE KEY `assetName` (`name`,`shortName`,`projectId`),
   ADD KEY `statusId` (`statusId`),
   ADD KEY `stageId` (`stageId`),
